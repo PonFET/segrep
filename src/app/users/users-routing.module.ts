f992d3280de38856717742e5f43ad5141a47827a
Tomás A.P. <tomas.ap@live.com>
2024-11-15 23:10:46 -0300

    File Structure
    Still structurin'

---
+++ src/app/users/users-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { NgModel } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+
+
+
+
+const routes: Routes = [
+
+    {
+        path: '',
+        //component: ,
+    },
+    {
+        path: '',
+        redirectTo: '',
+    }
+];
+
+@NgModule({
+
+    declarations: [],
+    imports: [],
+    exports: [],
+})
+
+export class UsersRoutingModule { }
